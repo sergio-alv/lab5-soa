53bbd47934563b354333212b694aaf5db08bd56d
sergio-alv <alvarezpeirosergio@gmail.com>
2020-12-16 01:09:38 +0100

    max:n supported

--- src/main/java/soa/eip/Router.java
+++ src/main/java/soa/eip/Router.java
@@ -12,8 +12,21 @@ public class Router extends RouteBuilder {
   public void configure() {
     from(DIRECT_URI)
       .log("Body contains \"${body}\"")
+      .process(exchange -> {
+        String regex = "max:[0-9]+";
+        String[] query = (exchange.getIn().getBody(String.class)).split(" ");
+        StringBuilder queryBody = new StringBuilder();
+        String countHeader = "5";
+        // Build new body and find count header
+        for (String keyword : query) {
+          if (keyword.matches(regex)) { countHeader = keyword.substring(4); }
+          else { queryBody.append(keyword).append(" "); }
+        }
+        exchange.getIn().setBody(queryBody.toString());
+        exchange.getIn().setHeader("count", countHeader);
+      })
       .log("Searching twitter for \"${body}\"!")
-      .toD("twitter-search:${body}")
+      .toD("twitter-search:${body}?count=${header.count}")
       .log("Body now contains the response from twitter:\n${body}");
   }
 }
